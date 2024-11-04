d76b157849f9830fa8c6339e4805b7c1f9551319
cx.li <chunxialims@163.com>
2024-11-03 18:24:01 -0800

    change ui

---  frontend/bitflow-frontend/components/PerformanceCard.tsx
+++  frontend/bitflow-frontend/components/PerformanceCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Card = styled.div`
@@ -71,10 +72,18 @@ interface PerformanceCardProps {
 }
 
 const PerformanceCard = ({ data }: PerformanceCardProps) => {
+  const [selectedModel, setSelectedModel] = useState('');
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedModel(event.target.value);
+  };
   return (
     <div>
-      <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4">
-        <option selected>Choose a country</option>
+      <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
+        value={selectedModel}
+        onChange={handleSelectChange}
+        >
+        <option value="" disabled>Choose a model</option>
         <option value="US">United States</option>
         <option value="CA">Canada</option>
         <option value="FR">France</option>
