b90eb3799478ad3f5e222a33291a1537ddafc14a
Kai <wangkai1209@gmail.com>
2024-11-05 19:45:24 -0800

    Update arima_function.py

--- bitcoin_prediction/arima_function.py
+++ bitcoin_prediction/arima_function.py
@@ -1,7 +1,7 @@
 # Best model:  ARIMA(0,1,1)
 # input parameter - start-date: 2000-00-00 
 #                   end-date: 2000-00-00
-# output -  mae                 Mean Absolute Error (MAE): 15672.115857495643
-#           mape                Mean Absolute Percentage Error (MAPE): 0.5330338931580348
+# output -  mae                 Mean Absolute Error (MAE): float 15672.115
+#           mape                Mean Absolute Percentage Error (MAPE): float x100 53.333%
 #           runtime             Runtime (millisecond): int
 #           pred_list           Predicted prices list
