1c0dcc5b78671fd643ced3d32724acf7b991f15f
kai wang <wangkai1209@gmail.com>
2024-11-05 18:33:41 -0800

    Arima algorithm para

---
+++ bitcoin_prediction/arima_function.py
@@ -0,0 +1,6 @@
+# Best model:  ARIMA(0,1,1)
+# input parameter - start-date: 2000-00-00 end-date: 2000-00-00
+# output -  ame                 Mean Absolute Error (MAE): 15672.115857495643
+#           mape                Mean Absolute Percentage Error (MAPE): 0.5330338931580348
+#           runtime             Runtime (seconds): 3.2271242141723633
+#           trend_path          Path to Trend Line Chart: Visualizes predicted vs. actual prices
---
+++ bitcoin_prediction/charts.py
@@ -0,0 +1,3 @@
+# Output  -     mae_chart_path          path to Error Bar Chart(Shows MAE for accuracy comparison)
+#               runtime_chart_path      path to Runtime Bar Chart(Displays efficiency of each algorithm)
+#               mape_chart_path         path to Dynamic Error Line Chart(Shows MAPE trends for each algorithm)
